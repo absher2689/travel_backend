5ccb946fbcd3aef7dde7a37295f53f815a880dfb
absher2689 <kcabsher@gmail.com>
2025-01-05 19:05:19 +0530

    Second commit

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ import authRoute from './routes/auth.js'
 import reviewRoute from './routes/reviews.js'
 import bookingRoute from './routes/bookings.js'
 
-
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 8000
